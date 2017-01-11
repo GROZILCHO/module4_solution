407d8f98d8088977ae1464e1e34ec7b2846a470d
Rossen Stanev <thespecials@gmail.com>
2017-01-11 23:50:52 +0200

    some final changes

--- SpeakHello.js
+++ SpeakHello.js
@@ -21,7 +21,7 @@
 	var helloSpeaker = {};
 	var speakWord = "Hello";
 
-	helloSpeaker.speak = function speak(name) {
+	helloSpeaker.speak = function (name) {
         console.log(speakWord + " " + name);
     }
     window.helloSpeaker = helloSpeaker;
